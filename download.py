baff0a8b340c613728f3927d1740c6740fcdc6c0
NicolasGHS <nicoghyselincks@outlook.be>
2024-01-28 00:23:20 +0100

    automated .deb and appImage

--- download.py
+++ download.py
@@ -8,8 +8,22 @@ path = os.path.join(home_dir, "Downloads")
 downloads_list = os.listdir(path)
 
 zip_extension = ".zip"
+deb_extension = ".deb"
+appImage_extension = ".AppImage"
+
 
 for file in downloads_list:
     if zip_extension in file:
         os.rename(f"{path}/{file}", f"{path}/zips/{file}")
         print(f"Succesfully moved {file}.")
+
+for file in downloads_list:
+    if deb_extension in file:
+        os.rename(f"{path}/{file}", f"{path}/debs/{file}")
+        print(f"Succesfully moved {file}.")
+
+for file in downloads_list:
+    if appImage_extension in file:
+        os.rename(f"{path}/{file}", f"{path}/appImages/{file}")
+        print(f"Succefully moved {file}.")
+
